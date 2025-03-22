d9f466e5e070fc6bf67da09664e364e4b12aa241
DominqueLawson <dominque.lawson115@gmail.com>
2025-03-21 22:22:56 -0700

    added github and resume buttons and hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,43 @@
+import heroBg from "../assets/herobg.svg";
 function Hero() {
   return (
-    <section id="hero" className="h-screen flex flex-col justify-center items-center text-center">
-      <h1 className="text-4xl font-bold">Hi, I'm Dominque Lawson</h1>
-      <p className="text-lg text-gray-400">Full Stack Software Engineer | AWS Certified</p>
-      <a href="#projects" className="mt-4 px-6 py-2 bg-purple-500 text-white rounded-lg">View My Work</a>
+    <section
+      id="hero"
+      className="relative h-screen flex flex-col justify-center items-center text-center bg-cover bg-center"
+      style={{ backgroundImage: `url(${heroBg})` }}
+    >
+      {/* Dark Overlay */}
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      {/* Content */}
+      <div className="relative z-10 text-white px-6">
+        <h1 className="text-4xl font-bold">Hi, I'm Dominque Lawson</h1>
+        <p className="text-lg text-gray-300 mt-2">
+          Full Stack Software Engineer | AWS Cloud Engineer | Google Cybersecurity Certified
+        </p>
+        <div className="flex flex-row justify-evenly">
+          <a
+          href="#projects"
+          className="mt-4 inline-block px-6 py-2 bg-purple-500 hover:bg-purple-600 text-white font-semibold rounded-lg transition duration-300 shadow-md"
+        >
+          View My Work
+        </a>
+        <a
+          href="/DominqueLawson-Resume.pdf"
+          download="DominqueLawson-Resume.pdf"
+          className="mt-4 inline-block px-6 py-2 bg-purple-500 hover:bg-purple-600 text-white font-semibold rounded-lg transition duration-300 shadow-md"
+        >
+          View My Resume
+        </a>
+        <a
+          href="https://github.com/DominqueLawson"
+          className="mt-4 inline-block px-6 py-2 bg-purple-500 hover:bg-purple-600 text-white font-semibold rounded-lg transition duration-300 shadow-md"
+        >
+          View My Github
+        </a>
+        </div>
+        
+      </div>
     </section>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/FSDLogo.svg";
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-gray-800 p-4 shadow-lg">
+    <nav className="fixed z-10 top-0 left-0 w-full p-4">
       <div className="container mx-auto flex justify-between items-center">
         <img src={logo} alt="Logo" />
         <div className="space-x-4">
